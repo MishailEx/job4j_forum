7aedd340c124b56e9df091b4ba5f5a3cf3e9e51a
MishailEx <Mishail.avram@mail.ru>
2022-03-17 16:35:39 +0300

    2. Spring boot security [#296071]

--- src/main/java/ru/job4j/forum/model/User.java
+++ src/main/java/ru/job4j/forum/model/User.java
@@ -1,11 +1,24 @@
 package ru.job4j.forum.model;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String password;
+
     private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+
+    private boolean enabled;
 
     public int getId() {
         return id;
@@ -31,14 +44,26 @@ public class User {
         this.username = username;
     }
 
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return id == user.id;
     }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -2,9 +2,9 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
-<c:if test="${not empty errorMessage}">
+<c:if test="${not empty errorMessge}">
     <div style="color:red; font-weight: bold; margin: 30px 0px;">
-            ${errorMessage}
+            ${errorMessge}
     </div>
 </c:if>
 <form name='login' action="<c:url value='/login'/>" method='POST'>
